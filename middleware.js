2f21ce3f4875f747ee07a8767a0313849ed4d0a5
Alzeks <89770121+Alzeks@users.noreply.github.com>
2024-04-23 11:06:46 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,15 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./app/authconfig";
 
-function myNextAuth(myConfig = { collback: { authorized() { return 6 + 8 } } }) {
-  const es = 9;
-  const authorizedData = myConfig.collback.authorized() + es
-  function auth() { return authorizedData }
-  return { auth, es, closureEs() { return es } }
-}
-const { auth, closureEs, es } = myNextAuth()
-console.log(auth(), closureEs(), es);
-
 export default NextAuth(authConfig).auth;
 
 export const config = {
